3919b191de7d93c5ae5a294c41f9bb68e04cb9ae
ihanfarhan <ihan.farhan@gmail.com>
2019-04-06 16:52:13 +0700

    Initial commit

---
+++ app/src/main/java/com/ihanfarhan/a10116914/Beres.java
@@ -0,0 +1,13 @@
+package com.ihanfarhan.a10116914;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Beres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beres);
+    }
+}
